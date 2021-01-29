291d41e35dac6d0c07587850a1577c0449148988
Vincent Engel <vincent.engel@huckit.de>
2021-01-29 16:43:50 +0100

    Added class HandlerIsolate

--- bin/srvls.dart
+++ bin/srvls.dart
@@ -4,40 +4,52 @@ import 'dart:isolate';
 
 import 'package:srvls/srvls.dart';
 
-Future<void> main(List<String> arguments) async {
-  final functionsDir = Directory('./functions');
+Future<void> main() async {
+  print('Starting...');
+  final handler = await HandlerIsolate.init(
+      File('./functions/forms/save.dart').absolute.path);
 
-  await functionsDir
-      .list(recursive: true)
-      .where((event) => event is File)
-      .forEach((element) {
-    print(element);
-  });
-
-  await schmain();
-}
-
-Future<void> schmain() async {
-  final receiverPort = ReceivePort();
-  final receiveStream = receiverPort.asBroadcastStream();
-
-  await Isolate.spawnUri(
-    Uri.parse('../functions/forms/save.dart'),
-    [],
-    receiverPort.sendPort,
-  );
-
-  final sendPort = await receiveStream.first as SendPort;
-
-  final rawRequest = Request(
+  final response = await handler.request(Request(
     path: '/forms/save',
     json: {'name': 'World'},
-  ).toJson();
+  ));
 
-  sendPort.send(rawRequest);
+  print(response.toJson());
+}
 
-  await receiveStream.listen((dynamic rawResponse) {
-    final response = Response.fromJson(rawResponse as Map<String, dynamic>);
-    print(response.toJson());
-  });
+class HandlerIsolate {
+  ReceivePort receiverPort;
+
+  Stream<dynamic> receiveStream;
+
+  SendPort sendPort;
+
+  HandlerIsolate._();
+
+  static Future<HandlerIsolate> init(String path) async {
+    final handler = HandlerIsolate._();
+    await handler._init(path);
+
+    return handler;
+  }
+
+  Future<void> _init(String path) async {
+    receiverPort = ReceivePort();
+    receiveStream = receiverPort.asBroadcastStream();
+
+    await Isolate.spawnUri(
+      Uri.parse(path),
+      [],
+      receiverPort.sendPort,
+    );
+
+    sendPort = await receiveStream.first as SendPort;
+  }
+
+  Future<Response> request(Request request) async {
+    sendPort.send(request.toJson());
+
+    final rawResponse = await receiveStream.first as Map<String, dynamic>;
+    return Response.fromJson(rawResponse);
+  }
 }
