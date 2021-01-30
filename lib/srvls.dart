9645fe9e29ece373c307e250b5ae4fabf2e4dae0
Vincent Engel <ve@vesion.io>
2021-01-30 16:38:48 +0100

    Implemented router and http server using shelf

--- lib/srvls.dart
+++ lib/srvls.dart
@@ -1,90 +1,29 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 
-typedef HandlerFunction = FutureOr<Response> Function(Request);
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_router/shelf_router.dart';
 
-class Response {
-  final dynamic json;
-  final Map<String, String> headers;
-  final int status;
+export 'package:shelf/shelf.dart';
 
-  Response.ok({
-    this.json,
-    this.headers = const {},
-  }) : status = 200;
-
-  Map<String, dynamic> toJson() => <String, dynamic>{
-        'status': status,
-        'headers': headers,
-        'json': json,
-      };
-
-  Response.fromJson(Map<String, dynamic> json)
-      : status = json['status'] as int,
-        headers = json['headers'] as Map<String, String>,
-        json = json['json'] as dynamic;
-}
-
-class Request {
-  final String method;
-  final String path;
-  final Map<String, String> headers;
-  final dynamic json;
-
-  Request({
-    this.method,
-    this.path,
-    this.headers,
-    this.json,
-  });
-
-  Request.fromJson(Map<String, dynamic> json)
-      : method = json['method'] as String,
-        path = json['path'] as String,
-        headers = json['headers'] as Map<String, String>,
-        json = json['json'] as dynamic;
-
-  Map<String, dynamic> toJson() => <String, dynamic>{
-        'method': method,
-        'path': path,
-        'headers': headers,
-        'json': json,
-      };
+extension SrvlsRequest on Request {
+  Future<dynamic> get json async => jsonDecode(await readAsString());
 }
 
 Future<void> run(
   List<String> args,
-  Map<String, HandlerFunction> routeHandlerMap,
+  Map<String, Function> routeHandlerMap,
 ) async {
-  var server = await HttpServer.bind(
-    InternetAddress.loopbackIPv4,
-    4040,
-  );
-  print('Listening on localhost:${server.port}');
+  final router = Router();
 
-  await for (HttpRequest request in server) {
-    final json =
-        request.headers.contentType.primaryType == ContentType.json.primaryType
-            ? await request
-                .cast<List<int>>()
-                .transform<String>(Utf8Decoder())
-                .transform(JsonDecoder())
-                .first
-            : null;
+  print('Starting...');
 
-    final headers = <String, String>{};
-    request.headers.forEach((name, values) {
-      headers[name] = values.join(',');
-    });
+  routeHandlerMap.forEach((route, handler) {
+    print('Adding $route');
+    router.all(route, handler);
+  });
 
-    final req = Request(
-      method: request.method,
-      path: request.uri.path,
-      headers: headers,
-      json: json,
-    );
-
-    await request.response.close();
-  }
+  final server = await io.serve(router, 'localhost', 8080);
+  print('Server running on port ${server.port}!');
 }
